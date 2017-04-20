153081e97e051a568c7411d8a878094f674cbfcf
fps01 <fps.06@mail.ru>
2017-04-20 11:12:33 +0300

    deleted unused model fields

--- src/views/_configuration.php
+++ src/views/_configuration.php
@@ -2,8 +2,3 @@
 /**
  * @var $this \yii\web\View
  */
-
-echo '<div class="box-body">';
-echo $form->field($model, 'itemsPerPage');
-echo $form->field($model, 'showHiddenInTree')->checkbox();
-echo '</div>';
